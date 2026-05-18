09c86c719198cf9838b636ac4de68df0980bd8b1
mubeen <mubeen@air.local>
2026-05-18 20:56:17 +0530

    initial commit

---
+++ src/main.py
@@ -0,0 +1,28 @@
+
+from langchain_ollama import ChatOllama
+
+
+
+
+def main():
+    llm = ChatOllama(
+    model="llama3.2",
+    temperature=0,
+    # other params...
+    )
+
+    messages = [
+        (
+            "system",
+            "You are a helpful assistant who is knowledgeable and answers user question",
+        ),
+        ("human", "write simple hello world in python"),
+    ]
+    ai_msg = llm.invoke(messages)
+    print()
+    print("=============")
+    print(ai_msg.content)
+
+
+if __name__ == "__main__":
+    main()
